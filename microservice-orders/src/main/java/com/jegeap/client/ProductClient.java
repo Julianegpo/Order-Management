a943ff0b828757118d3c9264d36fa36fc5e8c0e6
Julián Egea <julian.egea.ponce@gmail.com>
2024-12-11 00:19:06 +0100

    openFeign microservices connections
    openFeign from orders to products working

--- microservice-orders/src/main/java/com/jegeap/client/ProductClient.java
+++ microservice-orders/src/main/java/com/jegeap/client/ProductClient.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.jegeap.model.Product;
 
 @FeignClient(name="service-products")
-public class ProductClient {
+public interface ProductClient {
 
-	// Mapeamos a donde vamos a dirigir la peticion	(direccion del mapping objetivo)
-	@GetMapping("/buscar/{id}")
-	public Product buscar(@PathVariable(name = "id") Long id) {
-		return null;
-	}
+	@GetMapping("/list")
+	public Product getAll();
+	
+	@GetMapping("/{id}")
+	public Product getOneByProductId(@PathVariable Long id);
 	
 }
--- microservice-orders/src/main/java/com/jegeap/service/OrderServiceImpl.java
+++ microservice-orders/src/main/java/com/jegeap/service/OrderServiceImpl.java
@@ -3,17 +3,23 @@ package com.jegeap.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import com.jegeap.client.ProductClient;
 import com.jegeap.model.Order;
 import com.jegeap.repository.OrderRepository;
 
 @Service
+@Primary
 public class OrderServiceImpl implements OrderService {
 	
 	@Autowired
 	private OrderRepository repository;
-
+	
+	@Autowired
+	private ProductClient feignClient;
+	
 	@Override
 	@Transactional
 	public Order createOrder(Order order) {
